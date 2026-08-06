517277996935ddfcaebd390e7174ab7574cc0a1a
Shrihan <shrihan.edu@gmail.com>
2026-08-06 19:56:10 +0530

    chore: hide API keys and use environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3741514491-88d43",
-  "appId": "1:703437420:web:aac8a92dde079557bf1649",
-  "apiKey": "AIzaSyDwKkv0XEC9L8S67BDPmoBTv8OA08ihsus",
-  "authDomain": "studio-3741514491-88d43.firebaseapp.com",
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-3741514491-88d43",
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:703437420:web:aac8a92dde079557bf1649",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-3741514491-88d43.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "703437420"
 };
